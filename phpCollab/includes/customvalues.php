2bbba4c5a86908da7ddf25bce0bd3a2dc47b4b1f
root <root@get1.thegetmelist>
2015-09-04 15:57:10 -0400

    add

---
+++ phpCollab/includes/customvalues.php
@@ -0,0 +1,25 @@
+<?php
+#Application name: PhpCollab
+#Status page: 2
+#Path by root: ../includes/customvalues.php
+
+/**
+ * Check to see if the languageSession is set, if it is then
+ * check to see if there is a custom_xx file for it.  If not then default
+ * to English
+ */
+if ($languageSession == '') {
+    $languageSession = 'en';
+}
+
+/**
+ * Check to see if the languageSession file exists, if not then use English as fallback
+ */
+$customLanguageFile = "../languages/custom_" . $languageSession . ".php";
+
+if (file_exists($customLanguageFile)) {
+    include $customLanguageFile;
+} else {
+    include "../languages/custom_en.php";
+}
+?>
